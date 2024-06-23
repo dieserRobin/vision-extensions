850c0ee715c0b0bd4b48afacde49a2376e9689a9
Robin <mail@robin.software>
2024-06-23 23:37:33 +0200

    updated ui and added support for multiple matches

--- em-scores/src/pages/layer.tsx
+++ em-scores/src/pages/layer.tsx
@@ -1,7 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import render from "../app/render";
+import useStorageSettings from "../hooks/storage";
+import { useMemo } from "react";
 
-type GameData = {
+export type GameData = {
+  id: string;
   status: string;
   homeTeam: {
     name: string;
@@ -19,43 +22,44 @@ type GameData = {
 }
 
 const Layer = () => {
-  const match = useQuery<GameData>({
+  const { layerSettings } = useStorageSettings();
+
+  const match = useQuery<GameData[]>({
     queryKey: ["match"],
     queryFn: async () => {
       const response = await fetch("https://services.robin.software/v1/em");
-      return response.json() as Promise<GameData>;
+      return response.json() as Promise<GameData[]>;
     },
     refetchInterval: 1000 * 15,
   });
 
+  const currentMatch = useMemo(() => layerSettings?.selectedMatch ? match.data?.find((match) => match.id === layerSettings?.selectedMatch) : match.data?.[0], [layerSettings, match.data]);
+
   return (
     <main className="w-screen h-screen overflow-hidden">
       <div className="flex flex-col items-center justify-center h-full">
-        {match.data && (
-          <div className="flex flex-col items-center justify-center">
+        {currentMatch && (
+          <div className="flex flex-col items-center justify-center bg-gradient-to-r from-blue-700 to-blue-600 p-3 rounded-xl">
             <div className="flex items-center justify-center">
-              <img src={match.data.homeTeam.logo} alt={match.data.homeTeam.name} className="w-16 h-16" />
+              <img src={currentMatch.homeTeam.logo} alt={currentMatch.homeTeam.name} className="w-8 h-8" />
               <div className="flex flex-col gap-0">
                 <div className="text-white">
-                  <div style={{
-                    textShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
-                  }} className="text-sm mt-2 text-center uppercaser">{match.data.status === "UPCOMING" ? "Upcoming" : match.data.phase === "HALF_TIME_BREAK" ? "Half time break" : match.data.minute + "'"}</div>
                 </div>
-                <div className="text-white" style={{
-                  textShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
-                }}>
-                  <span className="text-2xl font-bold mx-4">{match.data.homeTeam.score}</span>
-                  <span className="text-2xl font-bold">-</span>
-                  <span className="text-2xl font-bold mx-4">{match.data.awayTeam.score}</span>
+                <div className="bg-white text-blue-500 rounded-full mx-2">
+                  <span className="text-2xl font-bold mx-4">{currentMatch.homeTeam.score}</span>
+                  <span className="text-2xl font-bold"></span>
+                  <span className="text-2xl font-bold mx-4">{currentMatch.awayTeam.score}</span>
                 </div>
               </div>
-              <img src={match.data.awayTeam.logo} alt={match.data.awayTeam.name} className="w-16 h-16" />
+              <img src={currentMatch.awayTeam.logo} alt={currentMatch.awayTeam.name} className="w-8 h-8" />
+
+              <div className="text-sm ml-2 text-center uppercase text-white">{currentMatch.status === "UPCOMING" ? "Upcoming" : currentMatch.phase === "HALF_TIME_BREAK" ? "Half time break" : currentMatch.phase === "FINISHED" ? "Finished" : currentMatch.minute + "'"}</div>
             </div>
           </div>
         )
         }
       </div>
-    </main>
+    </main >
   );
 };
 
--- em-scores/src/pages/settings.tsx
+++ em-scores/src/pages/settings.tsx
@@ -1,9 +1,53 @@
+import { useQuery } from "@tanstack/react-query";
 import render from "../app/render";
+import useStorageSettings from "../hooks/storage";
+import { GameData } from "./layer";
+import { useEffect, useState } from "react";
 
 const Settings = () => {
+  const { layerSettings, updateStorage } = useStorageSettings();
+
+  const match = useQuery<GameData[]>({
+    queryKey: ["match"],
+    queryFn: async () => {
+      const response = await fetch("https://services.robin.software/v1/em");
+      return response.json() as Promise<GameData[]>;
+    },
+    refetchInterval: 1000 * 15,
+  });
+
+  const [selectedMatch, setSelectedMatch] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (layerSettings) {
+      setSelectedMatch(layerSettings.selectedMatch);
+    }
+  }, [layerSettings]);
+
+  const handleMatchChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedMatch(event.target.value);
+    updateStorage({
+      layer: {
+        selectedMatch: event.target.value,
+      },
+    });
+  };
+
   return (
     <main className="w-screen h-screen overflow-hidden bg-[#171717] p-3">
-      <h1 className="text-xl text-center mt-8 text-white">The current active EM game will be displayed in the overlay.</h1>
+      <h3 className="text-white text-lg font-bold">Selected Match</h3>
+
+      <select
+        className="w-full p-2 mt-2 bg-[#1e1e1e] text-white rounded-md"
+        value={selectedMatch || ""}
+        onChange={handleMatchChange}
+      >
+        {match.data?.map((match) => (
+          <option key={match.startTime} value={match.id}>
+            {match.homeTeam.name} vs {match.awayTeam.name}
+          </option>
+        ))}
+      </select>
     </main>
   );
 };
